856b640ad36be6840eb12cd6463948e94f576bc6
jwhang627 <jaywhang627@gmail.com>
2020-05-28 18:54:05 -0500

    update

--- src/game.py
+++ src/game.py
@@ -1,9 +1,10 @@
+#!usr/bin/python3
 # src/game.py
 
 # Use this website for ascii conversion:
 # http://www.patorjk.com/software/taag/
 
-with open("./ascii/title.txt","r") as o:
+with open("./src/ascii/title.txt","r") as o:
     a = o.read()
 
 print(a)
